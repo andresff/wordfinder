489601f62b9723880615030113d67f547e315ac5
afalcon <afalcon@we-are-around.com>
2025-06-17 13:35:39 -0300

    Initial commit

---
+++ WordFinder.Test/WordFinderTest.cs
@@ -0,0 +1,67 @@
+﻿namespace WordFinder.Test;
+
+public class WordFinderTest
+{
+    private WordFinderLibrary.WordFinder _finder;
+
+    public WordFinderTest() 
+    {
+        List<string> matrix = new();
+        matrix.Add("chill");
+        matrix.Add("acwdo");
+        matrix.Add("chill");
+        matrix.Add("alndd");
+        matrix.Add("alddc");
+
+        _finder = new WordFinderLibrary.WordFinder(matrix);        
+    }
+
+    [Fact]
+    public void WithTwoWordsFound_ReturnTwoResult()
+    {
+        List<string> wordStream = new List<string>() {"chill", "cold", "word", "wind"};
+
+        var result = _finder.Find(wordStream);
+
+        Assert.Equal(2, result.Count());
+        Assert.Equal("chill", result.ElementAt(0));
+        Assert.Equal("wind", result.ElementAt(1));
+    }
+
+    [Fact]
+    public void WithRepeatedWordStrem_ReturnTwoResult()
+    {
+        List<string> wordStream = new List<string>() { "chill", "test", "chill", "cold", "word", "wind" };
+
+        var result = _finder.Find(wordStream);
+
+        Assert.Equal(2, result.Count());
+        Assert.Equal("chill", result.ElementAt(0));
+        Assert.Equal("wind", result.ElementAt(1));
+    }
+
+    [Fact]
+    public void WithThreeWordsFound_ReturnThreeResult()
+    {
+        List<string> wordStream = new List<string>() { "chill", "cold", "word", "win", "wind" };
+
+        var result = _finder.Find(wordStream);
+
+        Assert.Equal(3, result.Count());
+        Assert.Equal("chill", result.ElementAt(0));
+        Assert.Equal("win", result.ElementAt(1));
+        Assert.Equal("wind", result.ElementAt(2));
+    }
+
+    [Fact]
+
+    public void WithNoWordFound_ReturnEmptyResult()
+    {
+        List<string> wordStream = new List<string>() { "chilld", "coldd", "wordd", "windd" };
+        
+        var result = _finder.Find(wordStream);
+
+        Assert.Empty(result);
+    }
+}
+
---
+++ WordFinderLibrary/WordFinder.cs
@@ -0,0 +1,109 @@
+﻿using System.Text;
+
+namespace WordFinderLibrary;
+
+public class WordFinder
+{
+    private readonly IEnumerable<string> _matrix;
+
+    public WordFinder(IEnumerable<string> matrix)
+    {
+        _matrix = matrix;
+    }
+
+    public IEnumerable<string> Find(IEnumerable<string> wordstream)
+    {
+        int wordLength = _matrix.First().Length;
+
+        var uniqueWordstream = wordstream.Where(w => w.Length<= wordLength && !string.IsNullOrEmpty(w)).Distinct().ToArray().AsSpan();
+        var wordsFound = new List<string>();
+
+        // iterate through each character ( row -> , column [n] )
+        for (var rowIndex = 0; rowIndex < wordLength; rowIndex++)
+        {
+            for (var wordIndex = 0; wordIndex < wordLength; wordIndex++)
+            {
+                wordsFound.AddRange(WordFound(rowIndex, wordIndex, wordLength, uniqueWordstream));
+            }
+        }
+
+        if (wordsFound.Count == 0)
+        {
+            return Enumerable.Empty<string>();  
+        }
+
+        var wordFoundHistogram = new Dictionary<string, int>();
+        foreach (var word in wordsFound)
+        {
+            if (wordFoundHistogram.ContainsKey(word))
+            {
+                wordFoundHistogram[word]++;
+            }
+            else
+            {
+                wordFoundHistogram.Add(word, 1);
+            }
+        }
+
+        return wordFoundHistogram.OrderByDescending(x => x.Value).Take(10).Select(x => x.Key);
+    }
+
+    private IEnumerable<string> WordFound(int rowIndex, int wordIndex, int wordLength, ReadOnlySpan<string> targetWords)
+    {
+        var result = new List<string>();
+        result.AddRange(SearchHorizontal(rowIndex, wordIndex, wordLength, targetWords));
+        result.AddRange(SearchVertical(rowIndex, wordIndex, wordLength, targetWords));
+
+        return result;
+    }
+
+    private List<string> SearchHorizontal(int rowIndex, int wordIndex, int wordLength, ReadOnlySpan<string> targetWords)
+    {
+        ReadOnlySpan<char> wordTemp = _matrix.ElementAt(rowIndex).AsSpan();
+
+        var result = new List<string>();
+        for (int i = wordIndex, j = 1; i < wordLength; i++, j++)
+        {
+            foreach (var target in targetWords)
+            {
+                if (wordTemp.Slice(wordIndex, j).Equals(target, StringComparison.Ordinal))
+                {
+                    result.Add(target);
+                }
+            }
+        }
+
+        return result;
+    }
+
+    private List<string> SearchVertical(int rowIndex, int wordIndex, int wordLength, ReadOnlySpan<string> targetWords)
+    {
+        var result = new List<string>();
+        var verticalWords = GetVerticalWord(rowIndex, wordIndex, wordLength);
+        foreach (var vw in verticalWords)
+        {
+            if (targetWords.Contains(vw))
+            {
+                result.Add(vw);
+            }
+        }
+
+        return result;
+    }
+
+    private IEnumerable<string> GetVerticalWord(int rowIndex, int charIndex, int wordLength)
+    {
+
+        StringBuilder builder = new StringBuilder();
+
+        var verticals = new List<string>();
+
+        for (int i = rowIndex, j = 0; i < wordLength; i++, j++)
+        {
+            builder.Append(_matrix.ElementAt(i)[charIndex]);
+            verticals.Add(builder.ToString());
+        }
+
+        return verticals;
+    }
+}
